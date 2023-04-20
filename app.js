0661fd6f755ac7f457e76390539118a234716b26
rbuchthal25 <119367191+rbuchthal25@users.noreply.github.com>
2023-04-20 13:18:50 -0400

    small changes

--- app.js
+++ app.js
@@ -86,10 +86,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
-
-
-
 app.use(pw_auth_router)
 app.use(layouts);
 
